735e5e3e04556390597a82f6479d11bd34e6c593
colin.boegli <colin.boegli@divcom.ch>
2026-05-06 11:07:29 +0200

    ajout de la page Springfield

--- src/pages/a-propos.vue
+++ src/pages/a-propos.vue
@@ -1,8 +1,50 @@
 <template>
-  <v-container>
-    <h1 class="text-h3 text-center my-6">
-      À propos
+  <v-container class="py-6">
+    <h1 class="text-h3 text-center mb-6">
+      À propos de Springfield
     </h1>
-    <!-- Contenu à venir -->
+
+    <v-card class="pa-6">
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-img
+              src="@/assets/springfield-map.png"
+              height="500"
+              cover
+              class="rounded-lg"
+          />
+        </v-col>
+
+        <v-col cols="12" md="6">
+          <v-img
+              src="@/assets/springfield-city.png"
+              height="500"
+              cover
+              class="rounded-lg"
+          />
+        </v-col>
+      </v-row>
+
+      <v-card-title class="text-h4 mt-6">
+        Springfield
+      </v-card-title>
+
+      <v-card-text class="text-body-1">
+        Springfield est la ville fictive où vivent les personnages
+        de la célèbre série <strong>The Simpsons</strong>.
+
+        <br><br>
+
+        On y retrouve des lieux emblématiques comme :
+
+        <br><br>
+
+        • La maison des Simpson<br>
+        • La centrale nucléaire<br>
+        • Le Kwik-E-Mart<br>
+        • La taverne de Moe<br>
+        • L’école primaire de Springfield
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
